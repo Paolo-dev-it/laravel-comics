f5a3f97f0db09285dab64b4c07771816901d65dd
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2023-01-13 15:05:29 +0100

    Styling single card

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,23 +3,29 @@
 @section('page-title', 'DC Comics - Prodotto')
 
 @section('main-content')
-    <h2>Prodotto: {{ $prodotto_singolo['title'] }}</h2>
 
-    <div class="containerComic">
 
-        <div class="comicCard">
+    <div class="single-cont">
+        <h2> {{ $prodotto_singolo['title'] }}</h2>
+        <div class="price">
+            <div class="left-price">
+                <p> U.S. Price: {{ $prodotto_singolo['price'] }} </p>
 
-            <div>
-                <img src="{{ $prodotto_singolo['thumb'] }}" alt="">
-            </div>
-            <div>
-                <h5>{{ $prodotto_singolo['title'] }}</h5>
-                <p>{!! $prodotto_singolo['description'] !!}</p>
             </div>
 
-
+            <div class="right-price">
+                <p>available</p>
+                <p>check Availability</p>
+            </div>
         </div>
 
+        <div>
+            <img src="{{ $prodotto_singolo['thumb'] }}" alt="">
+        </div>
+        <div>
+            <h5>{{ $prodotto_singolo['title'] }}</h5>
+            <p class="card-desc">{!! $prodotto_singolo['description'] !!}</p>
+        </div>
     </div>
 
 @endsection
